ff5ebc73c698f28ec1b0ec9c5d4d2d05c07d9ca5
FlorianWoelki <FlorianWoelki@gmx.de>
2022-08-07 17:27:43 +0200

    feat: add input field with send button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { ReactComponent as PaperAirplanIcon } from './assets/icons/paper-airplane.svg';
 import importedMessages from './assets/messages.json';
 import { ChatMessage } from './components/ChatMessage';
 import { ChatWindow } from './components/ChatWindow';
+import { InputField } from './components/InputField';
 import { TypingIndicator } from './components/TypingIndicator';
 import { useMessageTypingEffect } from './hooks/useMessageTypingEffect';
 
@@ -22,6 +24,17 @@ const App = () => {
           ))}
           {isTyping && <TypingIndicator></TypingIndicator>}
         </div>
+        <InputField
+          placeholder="Please wait"
+          className="absolute left-0 bottom-0 mb-8 right-0 flex mx-auto w-10/12"
+        >
+          <button
+            type="button"
+            className="absolute right-0 mr-2 text-white bg-blue-500 p-2 rounded-full transition duration-150 ease-in-out hover:bg-blue-600"
+          >
+            <PaperAirplanIcon className="w-5 h-5" />
+          </button>
+        </InputField>
       </ChatWindow>
     </div>
   );
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,3 +1,20 @@
-export const InputField: React.FC = (): JSX.Element => {
-  return <input type="text" />;
+import { classNames } from '../util/classNames';
+
+interface Props {
+  placeholder?: string;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export const InputField: React.FC<Props> = (props): JSX.Element => {
+  return (
+    <div className={classNames('flex items-center', props.className)}>
+      <input
+        type="text"
+        placeholder={props.placeholder}
+        className="block py-4 pl-6 pr-12 w-full text-gray-900 sm:text-md shadow rounded-full border border-gray-100 focus:outline-none transition duration-150 ease-in-out focus:border-blue-500"
+      />
+      {props.children}
+    </div>
+  );
 };
