ca139c47a53e478311ca1b668621cd07cfc05a53
FlorianWoelki <FlorianWoelki@gmx.de>
2022-08-14 16:12:28 +0200

    test(e2e): add initial cypress setup

---
+++ cypress/e2e/App.cy.tsx
@@ -0,0 +1,5 @@
+describe('App', () => {
+  it('should coming soon', () => {
+    cy.visit('/');
+  });
+});
---
+++ cypress/support/e2e.ts
@@ -0,0 +1,5 @@
+// in cypress/support/index.ts
+// load type definitions that come with Cypress module
+/// <reference types="cypress" />
+
+import './commands';
