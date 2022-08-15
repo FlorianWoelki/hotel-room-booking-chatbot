9ae55459499311eab65455767f99e205c1ea59df
FlorianWoelki <FlorianWoelki@gmx.de>
2022-08-15 09:14:45 +0200

    test(comp): add more tests for ChatDatePicker

--- src/__tests__/ChatDatePicker.test.tsx
+++ src/__tests__/ChatDatePicker.test.tsx
@@ -1,6 +1,33 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ChatDatePicker } from '../components/ChatDatePicker';
+import { click } from '../test-utils/interactions';
 
 it('should run without crashing', () => {
   render(<ChatDatePicker />);
+});
+
+it('should trigger `onClickOutside` when clicked outside the date picker', () => {
+  expect.assertions(1);
+  const fn = jest.fn();
+  const { container } = render(<ChatDatePicker onClickOutside={fn} />);
+  click(container.firstChild);
+  expect(fn).toBeCalledTimes(1);
+});
+
+it('should trigger `onChange` when clicked on start date and end date', () => {
+  expect.assertions(3);
+  const fn = jest.fn();
+  const { container } = render(<ChatDatePicker onChange={fn} />);
+  const notDisabledElements = container.querySelectorAll(
+    '[aria-disabled=false]',
+  );
+  expect(notDisabledElements.length).toBeGreaterThan(2);
+
+  const startDate = notDisabledElements[0];
+  const endDate = notDisabledElements[1];
+
+  click(startDate);
+  expect(fn).toBeCalledTimes(1);
+  click(endDate);
+  expect(fn).toBeCalledTimes(2);
 });
--- src/__tests__/chatbot/chatbot.test.tsx
+++ src/__tests__/chatbot/chatbot.test.tsx
@@ -1,5 +1,11 @@
 import { getResponse } from '../../chatbot';
 
-it('should run without crashing', () => {
-  getResponse('');
+it('should respond with a positive found answer', () => {
+  const response = getResponse('hi');
+  expect(response).toMatch(/Hello!|Hi, how can I help you\?|Hello there 😀/);
+});
+
+it('should respond with a negative not found answer', () => {
+  const response = getResponse('test');
+  expect(response).toMatch(/I am not sure, what that means 😔/);
 });
