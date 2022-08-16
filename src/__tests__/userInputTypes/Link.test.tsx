3faddc6d032eddefbb70fb005a31976c1c35b1ed
FlorianWoelki <FlorianWoelki@gmx.de>
2022-08-16 09:05:38 +0200

    test(Link): add unit tests

--- src/__tests__/userInputTypes/Link.test.tsx
+++ src/__tests__/userInputTypes/Link.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import { Link } from '../../components/userInputTypes/Link';
+import { click } from '../../test-utils/interactions';
 
 it('should render without crashing', () => {
   render(
@@ -8,11 +9,54 @@ it('should render without crashing', () => {
         id: 'testId',
         messages: [],
         userInput: {
-          type: 'text',
+          type: 'link',
+          href: 'http://test.de',
         },
       }}
       isWaitingForInput={false}
       onSubmit={() => {}}
     />,
   );
+});
+
+it('should render `Please wait` instead of link', () => {
+  expect.assertions(1);
+  const { container } = render(
+    <Link
+      data={{
+        id: 'testId',
+        messages: [],
+        userInput: {
+          type: 'selection',
+          href: 'http://test.de',
+        },
+      }}
+      isWaitingForInput={false}
+      onSubmit={() => {}}
+    />,
+  );
+
+  expect(container.firstChild?.firstChild?.textContent).toBe('Please wait');
+});
+
+it('should trigger `onClick` when clicking on button', () => {
+  expect.assertions(1);
+  const onSubmit = jest.fn();
+  const { container } = render(
+    <Link
+      data={{
+        id: 'testId',
+        messages: [],
+        userInput: {
+          type: 'selection',
+          href: 'http://test.de',
+        },
+      }}
+      isWaitingForInput={true}
+      onSubmit={onSubmit}
+    />,
+  );
+
+  click(container.querySelector('a')?.firstChild!);
+  expect(onSubmit).toBeCalledTimes(1);
 });
