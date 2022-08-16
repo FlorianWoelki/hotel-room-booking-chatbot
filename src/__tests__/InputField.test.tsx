e7e7b27ece20b752c7975e0f30911fa6b4321e09
FlorianWoelki <FlorianWoelki@gmx.de>
2022-08-16 19:54:07 +0200

    test(InputField): add unit tests

--- src/__tests__/InputField.test.tsx
+++ src/__tests__/InputField.test.tsx
@@ -1,6 +1,66 @@
-import { render } from '@testing-library/react';
+import * as yup from 'yup';
+import {
+  act,
+  fireEvent,
+  render,
+  RenderResult,
+  screen,
+  waitFor,
+} from '@testing-library/react';
 import { InputField } from '../components/InputField';
+
+beforeAll(() => {
+  jest.useFakeTimers();
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
 
 it('should run without crashing', () => {
   render(<InputField />);
+});
+
+it('should set `isValid` to true when `validation` prop is undefined', () => {
+  const { container } = render(
+    <InputField validation={undefined}>
+      {({ isValid }) => (isValid ? <p>Valid</p> : <p>Not Valid</p>)}
+    </InputField>,
+  );
+
+  fireEvent.change(container.querySelector('input')!, {
+    target: { value: 'Test' },
+  });
+  expect(container.querySelector('p')?.textContent).toBe('Valid');
+});
+
+it('should set `isValid` to true when `validation` prop is email and value is an email', async () => {
+  const email = yup.string().email().required();
+  const { container } = render(
+    <InputField validation={email}>
+      {({ isValid }) => (isValid ? <p>Valid</p> : <p>Not Valid</p>)}
+    </InputField>,
+  );
+
+  fireEvent.change(container.querySelector('input')!, {
+    target: { value: 'test@test.de' },
+  });
+
+  await waitFor(() => {
+    expect(container.querySelector('p')?.textContent).toBe('Valid');
+  });
+});
+
+it('should set `isValid` to false when `validation` prop is email but value is not an email', () => {
+  const email = yup.string().email().required();
+  const { container } = render(
+    <InputField validation={email}>
+      {({ isValid }) => (isValid ? <p>Valid</p> : <p>Not Valid</p>)}
+    </InputField>,
+  );
+
+  fireEvent.change(container.querySelector('input')!, {
+    target: { value: 'Test' },
+  });
+  expect(container.querySelector('p')?.textContent).toBe('Not Valid');
 });
