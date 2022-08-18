2fa8eea56e88cbf74f9b472a271b93825917b20d
FlorianWoelki <FlorianWoelki@gmx.de>
2022-08-18 08:26:34 +0200

    test(CalendarInputField): add rest of the unit tests

--- src/__tests__/CalendarInputField.test.tsx
+++ src/__tests__/CalendarInputField.test.tsx
@@ -2,6 +2,10 @@ import { render } from '@testing-library/react';
 import { CalendarInputField } from '../components/CalendarInputField';
 import { click } from '../test-utils/interactions';
 
+const formatDate = (date: Date) => {
+  return date.toLocaleString('de-DE', { dateStyle: 'medium' });
+};
+
 it('should run without crashing', () => {
   render(<CalendarInputField />);
 });
@@ -25,4 +29,39 @@ it('should hide calendar when clicking outside of calendar', () => {
   // Click on the outside of the calendar.
   click(container.querySelector('button'));
   expect(container.querySelector('button')).toBeNull();
+});
+
+it('should hide calendar when clicking outside of calendar', () => {
+  const { container } = render(<CalendarInputField />);
+
+  // Make calendar visible.
+  click(container.firstChild?.firstChild!);
+  expect(container.querySelector('button')).not.toBeNull();
+
+  // Get all the available dates in the calendar.
+  const availableDates = container
+    .querySelector('[role=listbox]')
+    ?.querySelectorAll('[aria-disabled=false]');
+  expect(availableDates).not.toBeNull();
+  expect(availableDates?.length).toBeGreaterThan(2);
+
+  // Select the start and end dates in the calendar.
+  const startDate = availableDates![0];
+  const endDate = availableDates![1];
+
+  expect(startDate).not.toBeNull();
+  expect(endDate).not.toBeNull();
+
+  click(startDate);
+  click(endDate);
+
+  // Get the element that describes the range of the selected dates.
+  const p = container.querySelector('p');
+  expect(p?.textContent).not.toBeNull();
+
+  // Check if the element has the correct date format.
+  const start = new Date();
+  const end = new Date();
+  end.setDate(end.getDate() + 1);
+  expect(p?.textContent).toBe(`${formatDate(start)} - ${formatDate(end)}`);
 });
