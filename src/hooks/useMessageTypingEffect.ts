3919e781b08a4b4a3ac5f990df154f2d72206f85
FlorianWoelki <FlorianWoelki@gmx.de>
2022-08-18 09:52:54 +0200

    refactor: change message delay based on environment

--- src/hooks/useMessageTypingEffect.ts
+++ src/hooks/useMessageTypingEffect.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import messageJson from '../assets/messages.json';
 
 // Defines the message delay in milliseconds.
-const MESSAGE_DELAY = 1750;
+const MESSAGE_DELAY = import.meta.env.DEV ? 100 : 1750;
 
 interface Message {
   value: string;
